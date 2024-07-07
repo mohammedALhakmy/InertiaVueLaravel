78821e144d137bfd26722c0ea613c7eee174bb15
mohammedALhakmy <mohammed.devlopement.yemen@gmail.com>
2024-07-07 19:26:39 +0300

    this therred the videos

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,20 @@
+<template>
+    <h1>Hello word , {{ name}}</h1>
+    <ul>
+        <li v-for="index,framework of frameworks" v-text="index"></li>
+    </ul>
+</template>
+
+<script>
+export default {
+    name: "Welcome",
+    props:{
+        name: String,
+        frameworks:Array
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
-
+use Inertia\Inertia;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return Inertia::render('Home',[
+        'name' => "Developer",
+        'frameworks' => [
+            'Laravel', 'Vue' , "Inertia","TailwindCss"
+        ]
+    ]);
 });
+
