519602759986be4ccd4ef30a19055f77f69b44a6
mohammedALhakmy <mohammed.devlopement.yemen@gmail.com>
2024-07-07 22:38:35 +0300

    custome NavLink and feucs link in the click NavLink

--- resources/js/Shared/Nav.vue
+++ resources/js/Shared/Nav.vue
@@ -1,18 +1,36 @@
 <template>
     <nav class="mt-6">
         <ul class="list-disc">
-            <li><Link href="/" class="text-gray-900 hover:underline">Home</Link></li>
-            <li><Link href="Users" class="text-gray-900 hover:underline">Users</Link></li>
-            <li><Link href="Setting" class="text-gray-900 hover:underline">Setting</Link></li>
-            <li><Link href="logout" method="post" as="button" :data="{foo:'bar'}" class="text-gray-900 hover:underline">Logout</Link></li>
+<!--            :class="{'font-bold underline':$page.url === '/'}"-->
+            <li>
+<!--                <Link href="/" class="text-gray-900 hover:underline" :class="{'font-bold underline':$page.component === 'Home'}">Home</Link>-->
+                <NavLink href="/" :active="$page.component === 'Home'">Home</NavLink>
+            </li>
+<!--            :class="{'font-bold underline':$page.url === '/Users'}"-->
+<!--            :class="{'font-bold underline' :$page.url.startsWith('/Users')}"-->
+            <li>
+                <NavLink href="Users" :active="$page.component=== 'Users'">User</NavLink>
+<!--                <Link href="Users" class="text-gray-900 hover:underline" :class="{'font-bold underline' :$page.component === 'Users'}">Users</Link>-->
+            </li>
+<!--            :class="{'font-bold underline':$page.url === '/Setting'}"-->
+<!--            :class="{'font-bold underline' :$page.url.startsWith('/Setting')}"-->
+            <li>
+                <NavLink href="Setting" :active="$page.component === 'Setting'">Setting</NavLink>
+<!--                <Link href="Setting" class="text-gray-900 hover:underline" :class="{'font-bold underline' :$page.component === 'Setting' }" >Setting</Link>-->
+            </li>
+<!--            :class="{'font-bold underline':$page.url === 'logout'}"-->
+    <li>
+<!--        <Link href="logout" method="post" as="button" :data="{foo:'bar'}" class="text-gray-900 hover:underline" :class="{'font-bold underline':$page.url.startsWith('logout')}" >Logout</Link>-->
+        <NavLink href="logout" method="post" as="button" :active="$page.component === 'logout'">Logout</NavLink>
+    </li>
         </ul>
     </nav>
 </template>
 
 <script>
-import { Link } from "@inertiajs/vue3";
+import  NavLink  from "../Shared/NavLink.vue";
 export default {
-    components: { Link },
+    components: { NavLink },
     name: "Nav"
 }
 
---
+++ resources/js/Shared/NavLink.vue
@@ -0,0 +1,20 @@
+<template>
+    <Link class="text-gray-900 hover:underline" :class="{'font-bold underline':active}">
+        <slot/>
+    </Link>
+</template>
+
+<script>
+import { Link } from "@inertiajs/vue3";
+export default {
+    components :{ Link },
+    name: "NavLink",
+    props:{
+        active : Boolean,
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
